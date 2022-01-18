c0b39496e4d21b5d05e5674261535e3e3b5ec367
artDev <artbalan20@gmail.com>
2022-01-18 10:22:51 +0100

    premier jet page profil

--- inc/header.inc.php
+++ inc/header.inc.php
@@ -4,9 +4,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- BOOTSTRAP -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <!-- CSS -->
     <link rel="stylesheet" href="css/style.css">
-     <title>Eshop</title>
+
+    <!-- FONT AWESOME -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+
+    <title>Eshop</title>
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -24,43 +33,43 @@
         <a class="nav-link" href="<?= URL ?>voiture.php">Voiture</a>
      </li>
 
-     <li class="nav-item">
      <?php
 
 
      //=============================================//
      // Menu admin                                  //
      //=============================================//
-
      if(user_is_admin()){
+         // gestion voiture
+         echo '<li class="nav-item">';
          echo '<a class="nav-link" href="' . URL . 'admin/gestion_voiture.php">Gestion Voiture</a>';
          echo '</li>';
-
-
-
-         echo '<li class="nav-item">';
      }
-
-
-
-
 
      //=============================================//
      // menu de connexion, deconnexion, inscription //
      //=============================================//
-
      if(user_is_connected()){
-        echo '<a class="nav-link" href="'. URL . 'connexion.php?action=deconnexion">Deconnexion</a>';
+         // page profil
+         echo '<li class="nav-item">';
+         echo '<a class="nav-link" href="'. URL . 'profil.php">Profil</a>';
+         echo '</li>';
+         // page deconnexion
+         echo '<li class="nav-item">';
+         echo '<a class="nav-link" href="'. URL . 'connexion.php?action=deconnexion">Deconnexion</a>';
+         echo '</li>';
      } else {
-         echo '
-            <a class="nav-link" href="' . URL . 'inscription.php">Inscription</a>
-        </li>
-        <li class="nav-item">
-             <a class="nav-link " href="' . URL . 'connexion.php">Connexion</a>
-         ';
+         // page inscription
+         echo '<li class="nav-item">';
+         echo '<a class="nav-link" href="' . URL . 'inscription.php">Inscription</a>';
+         echo '</li>';
+         // page connexion
+         echo '<li class="nav-item">';
+         echo '<a class="nav-link " href="' . URL . 'connexion.php">Connexion</a>';
+         echo '</li>';
      }
      ?>
-     </li>
+
       </ul>
     </div>
   </div>
